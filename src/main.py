bc42217e734b1cc5e5489337b38041938e5dd067
Austin Brehm <austin.brehm@gmail.com>
2026-01-31 13:58:46 -0700

    generalize to swap out token symbols, calculate combos within price ratio

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ This is meant to run as a script.
 
 import sys
 
+import numpy as np
+
 import solver
 from utils import Utils
 from cmc_api import CoinMarketCapAPI
@@ -16,35 +18,67 @@ if __name__ == "__main__":
     api = CoinMarketCapAPI(api_key=key)
 
     # TODO: add logic to check for valid token symbols
-    tokens = {"BTC": 0, "ETH": 0}
+    # TODO: implement CLI to input parameters
+    FIRST_TOKEN = "POL"
+    SECOND_TOKEN = "DOGE"
+    HOLDINGS = {FIRST_TOKEN: 1000, SECOND_TOKEN: 10000}  # token amounts held
+    GOAL = 100_000  # in dollars
+
+    tokens = {FIRST_TOKEN: 0, SECOND_TOKEN: 0}
     for token in tokens:
         quotes = api.get_crypto_quotes(symbol=token)
-        price = round(quotes["data"][token][0]["quote"]["USD"]["price"], 2)
+        price = round(quotes["data"][token][0]["quote"]["USD"]["price"], 10)
         tokens[token] = price
 
-    # TODO: implement CLI to input parameters
-    GOAL = 100_000  # in dollars
-    BTC = 0.1  # amount of BTC
-    ETH = 3  # amount of ETH
+    print(
+        f"Current Prices: {FIRST_TOKEN}: ${tokens[FIRST_TOKEN]:,}, "
+        f"{SECOND_TOKEN}: ${tokens[SECOND_TOKEN]:,}"
+    )
+    current_ratio = tokens[FIRST_TOKEN] / tokens[SECOND_TOKEN]
+    #print(f"Current Price Ratio: {current_ratio:,} {SECOND_TOKEN} per {FIRST_TOKEN}")
+    token_prices = solver.calculate_prices(
+        GOAL, HOLDINGS[SECOND_TOKEN], HOLDINGS[FIRST_TOKEN], current_ratio
+    )
+    #print(
+    #    f"Prices Needed to Reach ${GOAL:,} at Current Ratio: "
+    #    f"{FIRST_TOKEN} = ${token_prices[0]:,}, {SECOND_TOKEN} = ${token_prices[1]:}"
+    #)
 
-    if BTC * tokens["BTC"] + ETH * tokens["ETH"] >= GOAL:
+    if (
+        HOLDINGS[FIRST_TOKEN] * tokens[FIRST_TOKEN]
+        + HOLDINGS[SECOND_TOKEN] * tokens[SECOND_TOKEN]
+        >= GOAL
+    ):
         print("Target portfolio value already met.")
         sys.exit()
 
-    btc_prices = [tokens["BTC"]]
-    eth_prices = [tokens["ETH"]]
-    for ratio in range(1, 101, 10):
-        token_prices = solver.calculate_prices(GOAL, ETH, BTC, ratio).round()
-        btc_price = round(token_prices[0], 2)
-        eth_price = round(token_prices[1], 2)
-        btc_prices.append(btc_price)
-        eth_prices.append(eth_price)
+    first_prices = [tokens[FIRST_TOKEN]]
+    second_prices = [tokens[SECOND_TOKEN]]
+    for ratio in np.arange(current_ratio / 10, current_ratio, current_ratio / 10):
+        token_prices = solver.calculate_prices(
+            GOAL, HOLDINGS[SECOND_TOKEN], HOLDINGS[FIRST_TOKEN], ratio
+        )
+
+        first_price = token_prices[0]
+        first_prices.append(first_price)
+
+        second_price = token_prices[1]
+        second_prices.append(second_price)
+
+        #print(
+        #    f"Price Ratio: {ratio:,}, {FIRST_TOKEN} = "
+        #    f"${first_price:,}, {SECOND_TOKEN} = ${second_price:,}"
+        #)
 
     # Plot the price combinations
+    print("Plotting token price combinations...")
+
     util.plot(
-        btc_prices,
-        eth_prices,
-        f"Token price combos to reach ${GOAL:,} with {BTC} BTC and {ETH} ETH",
-        "Future Price of BTC ($)",
-        "Future Price of ETH ($)",
+        first_prices,
+        second_prices,
+        f"Token price combos to reach ${GOAL:,} with "
+        f"{HOLDINGS[FIRST_TOKEN]} {FIRST_TOKEN} and "
+        f"{HOLDINGS[SECOND_TOKEN]} {SECOND_TOKEN}",
+        f"Future Price of {FIRST_TOKEN} ($)",
+        f"Future Price of {SECOND_TOKEN} ($)",
     )
