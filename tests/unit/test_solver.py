04a9bee53989b3094e4b933f8931fa5c1e4e637a
Austin Brehm <austin.brehm@gmail.com>
2026-02-22 13:39:20 -0700

    update assertion and scenarios in test_calculate_prices

--- tests/unit/test_solver.py
+++ tests/unit/test_solver.py
@@ -1,21 +1,24 @@
 """Tests for the solver module."""
 
 import pytest
-import numpy as np
 
 from src import solver
 
 
 @pytest.mark.parametrize(
-    "target, token_1, token_2, price_ratio, expected_prices",
+    "target, token_1, token_2, price_ratio",
     [
-        (100, 1.0, 1.0, 0.5, np.array([33.33, 66.67])),
-        (100, 2.0, 1.0, 0.5, np.array([25.0, 50.0])),
-        (100, 10.0, 1.0, 0.1, np.array([5.0, 50.0])),
+        (10, 10, 100, 0.1),
+        (100, 1.0, 1.0, 0.5),
+        (100, 2.0, 1.0, 0.5),
+        (100, 10.0, 1.0, 0.1),
+        (10_000, 1.0, 1.0, 1.0),
+        (56_879, 22, 3, 0.43),
+        (1_000_000, 2.0, 1.0, 0.5),
     ],
 )
-def test_calculate_prices(target, token_1, token_2, price_ratio, expected_prices):
+def test_calculate_prices(target, token_1, token_2, price_ratio):
     """Tests the calculate_prices function with various inputs."""
     result = solver.calculate_prices(target, token_1, token_2, price_ratio)
 
-    np.testing.assert_equal(np.round(result, decimals=2), expected_prices)
+    assert target == (float(result[0]) * token_1) + (float(result[1]) * token_2)
